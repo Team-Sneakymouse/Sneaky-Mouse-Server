d1d6574c71d00d67ca3bc8bc74d0d5cb57393e7f
Monica Moniot <mamoniot@protonmail.com>
2021-11-16 18:26:54 -0500

    rearranged comments

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 
 // #[macro_use]
 extern crate redis;
-// extern crate rocket;
 
 fn main() {
     let client = redis::Client::open("redis://127.0.0.1/").expect("could not connect");
     let mut con = client.get_connection().expect("could not connect");
 
-    // let _ : redis::Value = redis::cmd("SET").arg("my_key").arg(42i32).query(&mut con).expect("SET failed");
+    //This code sets key "my_key" to 42 in the redis server, then requests it back to be printed by the program
+    let _ : redis::Value = redis::cmd("SET").arg("my_key").arg(42i32).query(&mut con).expect("SET failed");
     let val : String = redis::cmd("GET").arg("my_key").query(&mut con).expect("GET failed");
     print!("{}", val);
 }
