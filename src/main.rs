5dc3630c95345b6d28fa5278ce756d29e0bf2d3f
Monica Moniot <mamoniot@protonmail.com>
2021-11-16 18:25:12 -0500

    removed redundant dependency

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+// #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
+// #[macro_use]
 extern crate redis;
+// extern crate rocket;
 
 fn main() {
-    rocket().launch();
-}
-
-
-#[get("/")]
-fn get_movies() -> String {
-
     let client = redis::Client::open("redis://127.0.0.1/").expect("could not connect");
     let mut con = client.get_connection().expect("could not connect");
 
     // let _ : redis::Value = redis::cmd("SET").arg("my_key").arg(42i32).query(&mut con).expect("SET failed");
     let val : String = redis::cmd("GET").arg("my_key").query(&mut con).expect("GET failed");
-    val
-}
-
-
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount(
-        "/movies",
-        routes![get_movies],
-    )
+    print!("{}", val);
 }
